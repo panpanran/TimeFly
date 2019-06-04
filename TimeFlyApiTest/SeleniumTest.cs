ee02cee378d4e72b2049cd0e9b90748574136718
panpanr <panpanr@gmail.com>
2019-06-04 16:46:52 -0400

    add selenium

--- TimeFlyApiTest/SeleniumTest.cs
+++ TimeFlyApiTest/SeleniumTest.cs
@@ -6,17 +6,17 @@ namespace TimeFlyApiTest
     [TestClass]
     public class SeleniumTest
     {
-        //[TestMethod]
-        //public void TakeScreenshot()
-        //{
-        //    string url = "https://3w.huanqiu.com/a/6b1c64/7N5nhVyjRkI?agt=20&tt_group_id=6696537495833674244";
-        //    string image = @"~/test.png";
-        //    var driver = new ChromeDriver(@"../../../TimeFlyApiTest");
-        //    driver.Navigate().GoToUrl(url);
-        //    var ss = driver.GetScreenshot();
-        //    ss.SaveAsFile(image, OpenQA.Selenium.ScreenshotImageFormat.Png);
-        //    driver.Close();
-        //}
+        [TestMethod]
+        public void TakeScreenshot()
+        {
+            string url = "https://3w.huanqiu.com/a/6b1c64/7N5nhVyjRkI?agt=20&tt_group_id=6696537495833674244";
+            string image = @"test.png";
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            var ss = driver.GetScreenshot();
+            ss.SaveAsFile(image, OpenQA.Selenium.ScreenshotImageFormat.Png);
+            driver.Close();
+        }
 
         [TestMethod]
         public void UnitTestShouldAlwaysWorkTest()
