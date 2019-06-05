256c556d92491794e0e25c5d75fd0e5f8c08fb10
panpanr <panpanr@gmail.com>
2019-06-04 20:08:43 -0400

    change relative path

--- TimeFlyApiTest/SeleniumTest.cs
+++ TimeFlyApiTest/SeleniumTest.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
 
 namespace TimeFlyApiTest
 {
@@ -11,7 +13,7 @@ namespace TimeFlyApiTest
         {
             string url = "https://3w.huanqiu.com/a/6b1c64/7N5nhVyjRkI?agt=20&tt_group_id=6696537495833674244";
             string image = @"test.png";
-            var driver = new ChromeDriver();
+            var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             driver.Navigate().GoToUrl(url);
             var ss = driver.GetScreenshot();
             ss.SaveAsFile(image, OpenQA.Selenium.ScreenshotImageFormat.Png);
