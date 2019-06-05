9a238b4ef12619cae4b6475895d682b5f390cafa
panpanr <panpanr@gmail.com>
2019-06-04 20:46:18 -0400

    try again

--- TimeFlyApiTest/SeleniumTest.cs
+++ TimeFlyApiTest/SeleniumTest.cs
@@ -1,8 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.IE;
 using System.IO;
 using System.Reflection;
 
@@ -11,69 +8,16 @@ namespace TimeFlyApiTest
     [TestClass]
     public class SeleniumTest
     {
-        private TestContext testContextInstance;
-        private IWebDriver driver;
-        private string appURL;
-
-        public SeleniumTest()
-        {
-        }
-
         [TestMethod]
-        [TestCategory("Chrome")]
-        public void TheBingSearchTest()
+        public void TakeScreenshot()
         {
-            driver.Navigate().GoToUrl(appURL + "/");
-            driver.FindElement(By.Id("sb_form_q")).SendKeys("Azure Pipelines");
-            driver.FindElement(By.Id("sb_form_go")).Click();
-            driver.FindElement(By.XPath("//ol[@id='b_results']/li/h2/a/strong[3]")).Click();
-            Assert.IsTrue(driver.Title.Contains("Azure Pipelines"), "Verified title of the page");
-        }
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        [TestInitialize()]
-        public void SetupTest()
-        {
-            appURL = "http://www.bing.com/";
-
-            string browser = "Chrome";
-            switch (browser)
-            {
-                case "Chrome":
-                    driver = new ChromeDriver();
-                    break;
-                case "Firefox":
-                    driver = new FirefoxDriver();
-                    break;
-                case "IE":
-                    driver = new InternetExplorerDriver();
-                    break;
-                default:
-                    driver = new ChromeDriver();
-                    break;
-            }
-
-        }
-
-        [TestCleanup()]
-        public void MyTestCleanup()
-        {
-            driver.Quit();
+            string url = "https://3w.huanqiu.com/a/6b1c64/7N5nhVyjRkI?agt=20&tt_group_id=6696537495833674244";
+            string image = @"test.png";
+            var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl(url);
+            var ss = driver.GetScreenshot();
+            ss.SaveAsFile(image, OpenQA.Selenium.ScreenshotImageFormat.Png);
+            driver.Close();
         }
 
         [TestMethod]
